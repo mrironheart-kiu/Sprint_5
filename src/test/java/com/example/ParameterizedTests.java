fde588d1439532bf5b72975220105a6d107ee247
Kostin Ivan <mrironheart@gmail.com>
2026-01-12 21:13:56 +0300

    Убрал @Mock в классе ParameterizedTests

--- src/test/java/com/example/ParameterizedTests.java
+++ src/test/java/com/example/ParameterizedTests.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Spy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParameterizedTests {
-    @Spy
     private Feline feline;
 
     @BeforeEach
