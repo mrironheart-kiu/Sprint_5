69de752877ccfefb09248582c4049ca142f52981
Kostin Ivan <mrironheart@gmail.com>
2026-01-12 20:55:15 +0300

    Заменил @Spy на @Mock в классе CatTest Исправил метод getFoodReturnsFoodListTest() в классе CatTest

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Spy;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -15,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 class CatTest {
     private Cat cat;
-    @Spy
+    @Mock
     private Feline feline;
 
     @BeforeEach
@@ -36,8 +37,10 @@ class CatTest {
     @DisplayName("Метод Cat.getFood() возвращает список питания для кошки")
     void getFoodReturnsFoodListTest() throws Exception {
         List<String> expectedResult = new ArrayList<>(List.of("Животные", "Птицы", "Рыба"));
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         List<String> actualResult = cat.getFood();
 
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
         assertEquals(expectedResult, actualResult);
     }
 }
