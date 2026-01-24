df9ead7de6822397d84eccd1a0b30d41ef160b0c
Kostin Ivan <mrironheart@gmail.com>
2026-01-24 12:19:40 +0300

    Добавил final переменные в классе Alex

--- src/main/java/com/example/Alex.java
+++ src/main/java/com/example/Alex.java
@@ -3,20 +3,25 @@ package com.example;
 import java.util.List;
 
 public class Alex extends Lion {
+
+    private final String NEW_YORK_ZOO = "Нью-Йоркский зоопарк";
+    private final List<String> ALEX_FRIENDS_LIST = List.of("Марти", "Глория", "Мелман");
+    private final int DEFAULT_KITTENS = 0;
+
     public Alex(Feline feline) throws Exception {
         super(feline, "Самец");
     }
 
     public List<String> getFriends() {
-        return List.of("Марти", "Глория", "Мелман");
+        return ALEX_FRIENDS_LIST;
     }
 
     public String getPlaceOfLiving() {
-        return "Нью-Йоркский зоопарк";
+        return NEW_YORK_ZOO;
     }
 
     @Override
     public int getKittens() {
-        return 0;
+        return DEFAULT_KITTENS;
     }
 }
